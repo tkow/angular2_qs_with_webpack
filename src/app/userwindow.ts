9d0e8c94a5e5fb7ab9c5ce6fe89a821b52b70de5
tkow <t.kusama0613@outlook.jp>
2016-10-03 09:07:30 +0900

    first_commit

---
+++ src/app/userwindow.ts
@@ -0,0 +1,11 @@
+export class UseWindow {
+  image_url:string;
+  image_name:string;
+  title:string;
+  title_color:string;
+  description:string;
+  image_background:string;
+  description_color:string;
+  background_color:string;
+  layout_type:string;
+}
